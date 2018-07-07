d733cff536000e17759ef3f86416b08793ce3951
jitender94136 <jitender94136@gmail.com>
2018-07-07 10:27:22 +0530

    first commit

---
+++ src/main/java/in/flexsol/productionschedule/ProductionscheduleApplication.java
@@ -0,0 +1,12 @@
+package in.flexsol.productionschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionscheduleApplication.class, args);
+	}
+}
